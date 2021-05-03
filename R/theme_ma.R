5a71d2e2e86e431a3c3143b4dbaa8478d732afe7
RobinKohrs <robin.kohrs@gmx.de>
2021-05-03 13:19:10 +0200

    replace title and subtitle

--- R/theme_ma.R
+++ R/theme_ma.R
@@ -11,11 +11,9 @@ theme_ma = function(base_size = 13, base_family = "Times", title_size = 18,
       plot.margin = unit(c(1,1,1,1), "cm"),
       panel.background = element_rect(colour = "black", size=1.2),
       panel.grid.minor = element_blank(),
-      plot.title = element_text(vjust = 3),
-      plot.subtitle = element_text(vjust = 2),
       axis.title = element_text(size = base_size),
-      plot.title = element_text(size = title_size, hjust = .5),
-      plot.subtitle = element_text(size = subtitle_size, hjust = .5, face = "italic"),
+      plot.title = element_text(size = title_size, hjust = .5, vjust=3),
+      plot.subtitle = element_text(size = subtitle_size, hjust = .5, face = "italic", vjust=2),
       strip.background = element_rect(fill="transparent"),
       strip.text = element_text(size = base_size, face = "italic")
     )
