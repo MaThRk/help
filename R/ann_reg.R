5816bfd2cffdece814fee095854ba580ab77d35a
RobinKohrs <robin.kohrs@gmx.de>
2021-05-26 11:14:42 +0000

    ann_reg

---
+++ R/ann_reg.R
@@ -0,0 +1,22 @@
+# More or LESS pretty formated linear model output
+
+
+#' @export
+
+ann_reg = function(x){
+
+  intercept = round(coef(x)[[1]],2)
+  slope = round(coef(x)[[2]],2)
+
+  if(!inherits(x, "lm")){ # for the quantile regression case
+    text = paste0("Y = ", intercept, " + ", slope, " * X")
+    return(text)
+  }else{ # for linear models also report the coefficient of determination
+    r = round(summary(x)$r.squared, 2)
+    s = ifelse(slope > 0, "+", "")
+    text = paste0("Y = ", intercept, s, slope, "X; ", "R^2 ", r)
+    return(text)
+  }
+}
+
+
