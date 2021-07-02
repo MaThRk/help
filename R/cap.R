8c50ce2689b1b4ac6a0ff8748dd83602e6649332
RobinKohrs <robin.kohrs@gmx.de>
2021-07-02 12:01:40 +0000

    add p315 captions functions

--- R/cap.R
+++ R/cap.R
@@ -15,3 +15,16 @@ cap = function(n_dry = NULL, daily_thresh = NULL){
   return(s)
 
 }
+
+#' p3 string
+#' export
+cap_3 = function(){
+  s = "**p3** (precipitation in the the days prior to the slide) [mm]"
+  return(s)
+}
+
+#' p3 string
+#' export
+cap_15 = function(){
+  s = "**p15** (precipitation in the 15 days prior to p3) [mm]"
+}
