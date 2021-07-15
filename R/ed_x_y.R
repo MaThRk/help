b1aeb16e6e47b15224e171e4f1efa5da0585aa8f
RobinKohrs <robin.kohrs@gmx.de>
2021-07-15 10:35:47 +0200

    consisten annotations

---
+++ R/ed_x_y.R
@@ -0,0 +1,11 @@
+#' x and y legend for ed plots to ensure consistency
+#'
+#' @export
+ed_x = function(){
+  return("Event-duration [ days ]")
+}
+
+#' @export
+ed_y = function(){
+  return("Cumulated event-rainfall [ mm ]")
+}
